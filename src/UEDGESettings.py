5924fd8c794133ea1aebde1d7f72b320dd7513a5
Jerome Guterl <guterlj@fusion.gat.com>
2020-07-30 09:57:27 -0700

    init

---
+++ src/UEDGESettings.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 22:09:43 2020
+
+@author: jguterl
+"""
+
+import sys,os
+import easygui
+import platform,inspect
+import configparser
+import getpass
+
+#from .UEDGEToolBox import LsFolder
+
+import numpy as np
+
+        
+    
+class UEDGESettings(UEDGEProjects):
+    
+    def __init__(self):
+        # This is the default path for the config file of the UEDGEToolPack. 
+        self.SettingsConfigFile=os.path.join(os.path.expanduser("~"),'.UedgeSettings')
+        # Getting settings from the config file
+        self.SettingsConfig=self.GetSettingsConfig()
+        # Show settings if loaded
+        if self.SettingsConfig is None:
+            print('No configuration found for the settings of the UEDGEToolPack. Run SetSettingsConfigFile() if you wish to use this pack.')
+        else:
+            print('# UEDGEToolPack settings loaded from {}'.format(self.SettingsConfigFile))
+            ShowSettingsConfig()
+        if self.SettingsConfig is not None and self.SettingsConfig.get('ProjectsConfigFile') is not None:
+            super().__init__(self.SettingsConfig['ProjectsConfigFile'])
+            self.CurrentProject=self.ProjectsConfig['CurrentProject']
+        ShowProject(self.CurrentProject)    
+        
+        
+        
+        
+        
+        
+        
+            
+            self.UserName=self.config['UEDGE'].get('UserName')
+            self.ProjectsConfig
+        self.Platform=self.GetPlatform()
+    def WriteSettingsConfig(self):
+        
+        
+
+
+
+    def ShowSettings(self):
+        '''Print current UEDGE configuration'''
+        print('******** UEDGE configuration ********')
+        print('Username:',self.UserName)
+
+
+    
+    def CheckUEDGEConfig(self):
+        
+        if not os.path.exists(self.ConfigFileName):
+            print ('Cannot find the config file ' + self.ConfigFileName +'....')
+            return None
+        config = configparser.ConfigParser()
+        try:
+            config.read(self.ConfigFileName)
+            return config
+        except: 
+            print('Cannot parse the config file:',self.ConfigFileName)
+            return None
+def GetPlatform():
+        PF={}
+        AllFunctions = inspect.getmembers(platform, inspect.isfunction)
+        for (n,f) in AllFunctions:
+            if not n.startswith('_'):
+                try: 
+                    PF[n]=f()
+                except:
+                    pass
+        return PF
+    
+    def GetSettingsConfig(self):
+           print('### Looking for the UEDGEToolPack configuration file:{}'.format(self.SettingsConfigFile))
+           
+           if self.SettingsConfigFile is None or not os.path.exists(self.SettingsConfigFile):
+               print ('Cannot find the configuration file for the UEDGEToolPack:"{}"'.format(self.SettingsConfigFile))
+               
+               if QueryYesNo('Do you want to create a new configuration file for the UEDGEToolPack?'):
+                   self.SetSettingsConfigFile()   
+               else:
+                       return None
+    
+           print('### Reading settings for the UEDGEToolPack from:{} ...'.format(self.SettingsConfigFile))
+           return ReadConfigFile(self.SettingsConfigFile)
+
+
+def CreateUEDGESettingsFile():
+        SettingsConfig = configparser.ConfigParser()
+            Settings['UserName']=input('Enter the username:')
+            if Settings['UserName'] is '':
+            Settings['UserName']=getpass.getuser()
+            Settings['Email']=input('Enter the email address:')
+            Settings['Institution']=input('Enter the institution:')
+            
+            if QueryYesNo('Are these settings correct?'):
+               self.SettingsConfigFile=os.path.join(os.path.expanduser("~"),'.UedgeSettings')
+               with open(self.SettingsConfigFile, 'w') as f:
+                   config.write(f)
+               print(' # Creation of the settings config file:',self.SettingsConfigFile)
+               print(' # The config file .UEDGEInfo has not been created in the home folder...') 
+               return 
+
+
+
+
+
+def QueryYesNo(question, default="no"):
+        """Ask a yes/no question via input() and return their answer.
+    
+        "question" is a string that is presented to the user.
+        "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
+    
+        The "answer" return value is True for "yes" or False for "no".
+        """
+        valid = {"yes": True, "y": True, "ye": True,
+                 "no": False, "n": False}
+        if default is None:
+            prompt = " [y/n] "
+        elif default == "yes":
+            prompt = " [Y/n] "
+        elif default == "no":
+            prompt = " [y/N] "
+        else:
+            raise ValueError("invalid default answer: '%s'" % default)
+    
+        while True:
+            sys.stdout.write(question + prompt)
+            choice = input().lower()
+            if default is not None and choice == '':
+                return valid[default]
+            elif choice in valid:
+                return valid[choice]
+            else:
+                sys.stdout.write("Please respond with 'yes' or 'no' "
+                                 "(or 'y' or 'n').\n")  
+                
+Settings=UEDGESettings()
+S=Settings
+
+def CdRunDir():
+    os.chdir(Settings.RunDir)
+    print('Current working directory:',os.getcwd())
+ 
+def CdSaveDir():
+    os.chdir(Settings.SaveDir)
+    print('Current working directory:',os.getcwd())
+    
+def CdInputDir():
+    os.chdir(Settings.InputDir)
+    print('Current working directory:',os.getcwd())
+    
+def LsFolder(Folder,Filter='*',Ext="*.py",LoadMode=False):
+    import glob
+    if '.' in Ext:
+        ListFile = [f for f in glob.glob(os.path.join(Folder,Ext))] +[f for f in glob.glob(os.path.join(Folder,Filter)) if os.path.isdir(f)]
+    else:
+        ListFile = [f for f in glob.glob(os.path.join(Folder,Ext))]
+    Listfile=list(dict.fromkeys(ListFile).keys())
+    ListFile.sort(key=str.casefold)
+    print('### Content matching "{}" in {}:'.format(Ext,Folder))
+    if ListFile is not None:
+        for i,F in enumerate(ListFile):
+            print(' [{}]: {}'.format(i,os.path.basename(F))) 
+    print('')
+    Message='Enter a number to look into a folder or file or press r (return) or q (exit)\n >>>: '
+    Input=input(Message)
+    while Input!='q' and Input!='r':
+        if Input.isnumeric() and ListFile is not None and int(Input) in range(len(ListFile)):
+            if os.path.isfile(ListFile[int(Input)]):
+                print('File:{}'.format(ListFile[int(Input)]))
+                if LoadMode:
+                    return ListFile[int(Input)]
+                Input=input(Message)
+            elif os.path.isdir(ListFile[int(Input)]):
+                Input=LsFolder(os.path.join(Folder,ListFile[int(Input)]),Filter,Ext,LoadMode)
+                if Input=='r':
+                   for i,F in enumerate(ListFile):
+                       print(' [{}]: {}'.format(i,os.path.basename(F))) 
+                   print('') 
+                   Input=input(Message) 
+                elif LoadMode  and Input!='q':
+                    return Input
+        else:
+            Input=input(Message)
+    if Input=='q' and LoadMode:
+        return None
+    else:
+        return Input
+        
+    
+        
+def LsInputDir(Folder=None,Ext='*.py'):
+    if Folder is None:
+        LsFolder(Settings.InputDir,Ext)
+    else:
+        LsFolder(os.path.join(Settings.InputDir,Folder),Ext)
+        
+def LsSaveDir(Folder=None,Ext='*'):
+    if Folder is None:
+        LsFolder(Settings.SaveDir,Ext)
+    else:
+        LsFolder(os.path.join(Settings.SaveDir,Folder),Ext)
+    
+def Config():
+    Settings.Config() 
+    
+def InitConfig():
+    CreateUEDGESettingsFile()  
+# start into the run folder
+CdRunDir()    
---
+++ src/UEDGEToolBox.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 10:46:09 2020
+
+@author: jguterl
+"""
+import uedge
+import os
+from uedge import *
+from .UEDGESettings import *
+
+def GetListPackage()->list:
+    import pkgutil
+    import uedge
+    ListPkgUEDGE=[]
+    package = uedge
+    PkgList=list(pkgutil.iter_modules(package.__path__))
+    for pkg in PkgList:
+        PkgName=pkg.name
+        if PkgName.endswith('py'):
+            ListPkgUEDGE.append(PkgName[:-2])
+   
+    return ListPkgUEDGE
+
+def CheckFileExist(FilePath:str)->bool:
+    import os
+    if os.path.isfile(FilePath):
+        out=' '
+        while out!='y' and out!='n' and out!='': 
+            out=input('The file {} already exists. Do you want to overwrite it? [y]/n'.format(FilePath))
+            if out=='y' or out=='':
+                return True
+            elif out=='n':
+                return False            
+    else:
+        return True        
+
+    ListPkgUEDGE=[]
+    package = uedge
+    PkgList=list(pkgutil.iter_modules(package.__path__))
+    for pkg in PkgList:
+        PkgName=pkg.name
+        if PkgName.endswith('py'):
+            ListPkgUEDGE.append(PkgName[:-2])
+   
+    return 
+
+def Source(ObjectName:str=None,Folder:str='InputDir',Enforce=True,Verbose:bool=False,CaseName=None,CheckExistence=True,CreateFolder=False):
+    """
+    
+
+    Args:
+        ObjectName (str): DESCRIPTION.
+        Folder (str, optional): DESCRIPTION. Defaults to 'InputDir'.
+        Enforce (TYPE, optional): DESCRIPTION. Defaults to True.
+        Verbose (bool, optional): DESCRIPTION. Defaults to False.
+        CaseName (TYPE, optional): DESCRIPTION. Defaults to None.
+        CheckExistence (TYPE, optional): DESCRIPTION. Defaults to True.
+
+    Raises:
+        IOError: DESCRIPTION.
+
+    Returns:
+        ObjectPath (TYPE): DESCRIPTION.
+
+    """
+
+    if Verbose:
+        print('# Looking for input file {} in {}'.format(ObjectName,Folder))
+    if Folder=='InputDir':
+        try:
+            ObjectDir=Settings.InputDir
+        except: 
+            print('# Settings object for UEDGE not find... Looking for InputDir in current directory')
+            ObjectDir='InputDir'        
+    elif Folder=='RunDir':
+        try:
+            ObjectDir=Settings.CurrentProject['RunDir']
+        except: 
+            print('# Settings object for UEDGE not find... Looking for RunDir in current directory')
+            ObjectDir='RunDir'
+    elif Folder=='SaveDir':
+        try:
+            ObjectDir=Settings.CurrentProject['SaveDir']
+        except: 
+            print('# Settings object for UEDGE not find... Looking for SaveDir in current directory')
+            ObjectDir='SaveDir'
+    elif Folder is None:
+        ObjectDir=None
+    else:    
+        ObjectDir=Folder
+        
+    if ObjectName is None:
+        return ObjectDir
+    if ObjectDir is None:    
+        ObjectPath=os.path.abspath(ObjectName)
+    else:
+        if CaseName is  not None:
+            ObjectDir=os.path.join(ObjectDir,CaseName)
+            if not os.path.isdir(ObjectDir) and CreateFolder:
+                try:
+                    os.mkdir(ObjectDir)
+                except OSError:
+                    pass
+                    #print ("Creation of the directory {} failed".format(ObjectDir))
+        ObjectPath=os.path.join(os.path.abspath(ObjectDir),ObjectName)
+        
+    if CheckExistence and not os.path.exists(ObjectPath):
+        if Enforce:
+            raise IOError('Cannot find {}:'.format(ObjectPath))
+        else:
+            print('### Cannot find {}'.format(ObjectPath))
+        return None
+    else:
+        if Verbose:
+            print('### Found {}'.format(ObjectPath))
+        return ObjectPath 
+        
+        
+def PrintSummary():
+    print('******** Summary **********')
+    print('* neq  :',bbb.neq)
+    print('* nisp :',com.nisp)
+    print('* nhsp :',com.nhsp)
+    print('* ngsp :',com.ngsp)
+    print('* nzspt:',com.nzspt)
+    print('* nusp :',com.nusp)
+    print('* iigsp:',bbb.iigsp)
+    print('***************************')
+    
+def MakeTrajectories(Npoints,Data):
+        for k,v in Data.items():
+            Min=v['Min'] 
+            Max=v['Max']
+            x=np.linspace(0,1,Npoints)
+            f=v['f']
+            Data[k]=Min+f(x)*(Max-Min)
+        return Data
+
+    
+        
+# def GetCaseFolder(CaseName:str):
+#     if CaseName=='Default':
+#         CaseName=UEDGESimulation.GetTag()
+    
+#         elif         
+# def
+#  PathFile(FileName:str,SaveFolder:str='SaveDir',CaseName='Default',Enforce=True,Verbose:bool=False):
+    
+#         CaseFolder=GetCaseFolder(CaseFolder)
+            
+#          if os.path.dirname(FileName)!=''
+#         if Verbose:
+#             print('### Looking for input file {} in {}'.format(FileName,Folder))
+#         if Folder=='SaveDir':
+#             try:
+#                 FileDir=Settings.InputDir
+#             except: 
+#                 print('Settings object for UEDGE not find... Looking for SaveDir in current directory')
+#                 FileDir='SaveDir'
+#         elif Folder is None:
+#             ObjectDir=None
+#         else:
+#             ObjectDir=Folder
+            
+#         if ObjectDir is None:    
+#             FilePath=os.path.abspath(FileName)
+#         else:
+#             FilePath=os.path.join(os.path.abspath(ObjectDir),FileName)
+            
+#         if not os.path.exists(FilePath):
+#             if Enforce:
+#                 raise IOError('Cannot find {}:'.format(FiletPath))
+#             else:
+#                 print('### Cannot find {}'.format(ObjectPath))
+#             return None
+#         else:
+#             if Verbose:
+#                 print('### Found {}'.format(ObjectPath))
+#             return ObjectPath
